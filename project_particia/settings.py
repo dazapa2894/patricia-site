1059c09e3ffa233efac8acb69460a2ec6ee84e53
David <dazapa2893@gmail.com>
2016-04-27 15:36:01 -0500

    desactive el Debbug xD

--- project_particia/settings.py
+++ project_particia/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 SECRET_KEY = 'g6=zd9-5!#hfrzus5gigp+by!mffb-@+y1#=&ac2w3iel-!hog'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = []
 
